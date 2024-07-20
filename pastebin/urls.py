75e8d30796ddd61c095a76da42ff57c657aceefa
princejeanluc <princejeanluc.denteppe@gmail.com>
2024-07-20 13:03:15 +0100

    Update urls.py

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
